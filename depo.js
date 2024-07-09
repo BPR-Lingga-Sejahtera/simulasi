722777f22a484bea8c09d3e9e5d773a1ce4b7cf7
BPR-Lingga-Sejahtera <bprls_sampit@bprlinggasejahtera.co.id>
2024-07-09 16:27:38 +0700

    update depo

--- depo.js
+++ depo.js
@@ -1,20 +1,23 @@
 function calculateDepo() {
     var jumlahStoran = parseFloat(document.getElementById("jumlahStoran").value.replace(/[^\d]/g, '').toString());
+    var jumlahHari = parseFloat(document.getElementById("jumlahHari").value);
     var sukuBungaDepo = parseFloat(document.getElementById("sukuBungaDepo").value);
 
-    displayDepoResult(sukuBungaDepo, jumlahStoran);
+    console.log(jumlahHari)
+
+    displayDepoResult(sukuBungaDepo, jumlahStoran, jumlahHari);
 }
 
-function displayDepoResult(sukuBungaDepo, jumlahStoran) {
+function displayDepoResult(sukuBungaDepo, jumlahStoran, jumlahHari) {
     var resultDiv = document.getElementById("depo");
     var tableContent = "<div class='table-responsive'>";
     tableContent += "<table class='table'><tbody>";
 
-    var hasil = parseFloat((jumlahStoran * (sukuBungaDepo / 100) * 0.8 * 30 ) / 365);
+    var hasil = parseFloat((jumlahStoran * (sukuBungaDepo / 100) * 0.8 * jumlahHari ) / 365);
 
     tableContent += "<tr><td>Jumlah Storan</td><td>" + formatRupiahIDR(jumlahStoran)+ "</td></tr>";
     tableContent += "<tr><td>Suku Bunga</td><td>" + sukuBungaDepo + "%</td></tr>";
-    tableContent += "<tr><td>Tenor (Hari)</td><td>30</td></tr>";
+    tableContent += "<tr><td>Tenor (Hari)</td><td>" + jumlahHari + "</td></tr>";
     tableContent += "<tr><td>Bunga Perbulan</td><td>" + formatRupiahIDR(hasil) + "</td></tr>";
 
     tableContent += "</tbody></table></div>";
